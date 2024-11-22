70158d8e7069198952da10f58db2c909985252b4
Thariss <thariss.thavakumar@gmail.com>
2024-11-22 13:39:33 -0500

    Completed my footer section and header

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template> 
+    <div class= header> 
+        <h1> BlockFlix</h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+.header {
+  background-color: black; 
+  padding: 20px;  
+  width: 100%;  
+  position: fixed;  
+  top: 0;  
+  left: 0;  
+  z-index: 1000;  
+}
+
+h1 {
+  color: white; 
+  background-color: black;
+  font-family: 'Arial', sans-serif;
+  font-size: 3rem; 
+  font-style: italic;
+  text-align: center;
+  margin: 0;
+
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template> 
+    <body>
+        <div class="topnav">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">New Content</a>
+            <a href="#contact">Who's Watching</a>
+            <a href="#about">Settings</a>
+        </div>
+        <div class="herosection">
+            <img src="/src/assets/cinema.jpg" alt="Hero Image" class="hero-section">
+        <div class="myherosection">Welcome To BlockFlix</div>
+        </div>
+            <div class="collage">
+                <img src="/src/assets/collage.jpg" alt="Collage" class="collage">
+            <div class="my-collage"> Unlimited stories one destination </div>
+        </div>
+        <div class="signbuttons">
+    <button type="button" onclick="alert('Hello, enter your account details!')">Sign In</button>
+    <button type="button" onclick="alert('Dont have an account? Sign Up Now!')">Sign Up</button>
+        </div>
+    </body>
+</template>
+
+<style scoped>
+
+.hero-section {
+  position: center;
+  width: 100%
+}
+.collage {
+  opacity: 70%;
+  position: center;
+  width: 100%;
+}
+
+.my-collage {
+  font-size: 75px;
+  color: white;
+  font-family: "Bebas Neue", sans-serif;
+  position: center;
+  text-align: center;
+  top: 20%;
+  transform: translate(5%, -400%);
+}
+
+
+</style>
